d663c30f01a47fd6861c60e9d2e68630e6869d0d
ezzygarmyz <dragkhai@gmail.com>
2025-01-30 21:26:42 +0100

    bump to version 1.0.7

--- src/app.py
+++ src/app.py
@@ -99,7 +99,7 @@ def main():
         app_id = "com.btcz",
         home_page = "https://getbtcz.com",
         author = "BTCZCommunity",
-        version = "1.0.6"
+        version = "1.0.7"
     )
     return app
 
