cd5e2eec1e28aa5ee124a21243a4d093c4b9ba41
ezzygarmyz <dragkhai@gmail.com>
2025-08-20 04:25:36 +0100

    bump to version 1.3.5

--- BTCZWallet/__main__.py
+++ BTCZWallet/__main__.py
@@ -145,7 +145,7 @@ def main():
         app_id = "com.btcz",
         home_page = "https://getbtcz.com",
         author = "BTCZCommunity",
-        version = "1.3.4"
+        version = "1.3.5"
     )
     app.main_loop()
 
