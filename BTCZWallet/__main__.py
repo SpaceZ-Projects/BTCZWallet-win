89ac175d76e4dadd438c6bc3bd161895aa7f6b4b
ezzygarmyz <dragkhai@gmail.com>
2025-12-23 16:25:59 +0100

    bump to version 1.5.9

--- BTCZWallet/__main__.py
+++ BTCZWallet/__main__.py
@@ -299,7 +299,7 @@ def main():
         app_id = "com.btcz",
         home_page = "https://getbtcz.com",
         author = "BTCZCommunity",
-        version = "1.5.8"
+        version = "1.5.9"
     )
 
     uri_path = Os.Path.Combine(str(app.paths.cache), 'btcz_uri.txt')
